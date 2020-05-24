a76c4147fdd67c2ca55826a0b7b75df019cef6a6
akbalsedat <akbalsedat@gmail.com>
2020-05-24 16:03:28 -0400

    More Tests are added to ORDStest file

--- fall-2019-online-rest-assured-testing/src/test/java/com/automation/tests/day2/ORDStests.java
+++ fall-2019-online-rest-assured-testing/src/test/java/com/automation/tests/day2/ORDStests.java
@@ -1,4 +1,33 @@
 package com.automation.tests.day2;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class ORDStests {
+
+    String BASE_URL = "http://3.85.41.58:1000/ords/hr";
+
+    @Test
+    @DisplayName("Get list of all employees")
+    public void getAllEmployees(){
+     Response response = given().
+                baseUri(BASE_URL).
+             when().
+                get("/regions").prettyPeek();
+    }
+
+    @Test
+    @DisplayName("Get employee under specific ID")
+    public void getOneEmployee(){
+        Response response = given().
+                baseUri(BASE_URL).
+                when().
+                get("/employees/{id}", 100).prettyPeek();
+
+        response.then().statusCode(200); // to verify that status code is 200
+
+    }
 }
