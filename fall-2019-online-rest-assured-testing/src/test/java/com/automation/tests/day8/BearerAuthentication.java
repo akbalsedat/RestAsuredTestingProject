4b878751fdb663cfc42b11a260eefa3cdd2af373
akbalsedat <akbalsedat@gmail.com>
2020-05-28 15:30:29 -0400

    day8 package with BearerAuthentication file is added.

---
+++ fall-2019-online-rest-assured-testing/src/test/java/com/automation/tests/day8/BearerAuthentication.java
@@ -0,0 +1,22 @@
+package com.automation.tests.day8;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class BearerAuthentication {
+
+    @BeforeAll
+    public static void setup() {
+        baseURI = "https://cybertek-reservation-api-qa.herokuapp.com/";
+    }
+    @Test
+    public void loginTest(){
+        given().
+                queryParam("email", "teacherva5@gmail.com").
+                queryParam("password", "maxpayne").
+        when().
+                get("/sign").prettyPeek();
+    }
+}
