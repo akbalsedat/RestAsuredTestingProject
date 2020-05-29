c5cc06ffb8f543415574dffb76d52c4a98b1c886
akbalsedat <akbalsedat@gmail.com>
2020-05-29 09:50:34 -0400

    features and steps packages are added.

--- fall-2019-online-rest-assured-testing/src/test/java/com/automation/tests/day9/SchemaValidation.java
+++ fall-2019-online-rest-assured-testing/src/test/java/com/automation/tests/day9/SchemaValidation.java
@@ -1,11 +1,14 @@
 package com.automation.tests.day9;
 
 import com.automation.utilities.ConfigurationReader;
+import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
@@ -23,5 +26,20 @@ public class SchemaValidation {
         File schema = new File("spartan_schema.json");
         get("/spartans/{id}", 35).prettyPeek().then().
                 body(JsonSchemaValidator.matchesJsonSchema(schema));
+    }
+
+    @Test
+    public void schemaValidationListTest(){
+        File schemaFile = new File("spartan_schema.json");
+
+        Response response = given().accept(ContentType.JSON).when().get("/spartans");
+        List<Integer> id_collection = response.jsonPath().getList("id");
+
+        for (int index = 0; index < id_collection.size(); index++) {
+            get("/spartans/{id}", id_collection.get(index)).
+                    then().
+                            body(JsonSchemaValidator.matchesJsonSchema(schemaFile));
+            System.out.println("Spartan with " + id_collection.get(index) + " id passed schema validation.");
+        }
     }
 }
